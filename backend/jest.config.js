03758c7f8692ead82d5e256d4a96c6faf628215a
truongvinh1612 <truongvinh1612@gmail.com>
2026-05-10 00:39:56 +0700

    update job latest version

--- backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "node",   // ensures Node.js APIs are available
-  verbose: true,             // shows detailed test results
-  setupFiles: ["./jest.setup.js"],
-  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"], 
-  // looks for tests in __tests__ folders or files ending with .test.js/.spec.js
-};
